2a67795d86aec015bd27e238ec9d14b9f2004ef3
weiliuyi <weiliuyivip@163.com>
2020-07-04 14:46:33 +0800

    classloader

---
+++ src/main/java/example/classloader/LoaderTest.java
@@ -0,0 +1,27 @@
+package example.classloader;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+public class LoaderTest {
+    @Test
+    public void test1 () {
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<BootStrap ClassLoader>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Stream.of(System.getProperty("sun.boot.class.path").split(";")).
+                forEach(System.out::println);
+
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<ExtClassLoader>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Stream.of(System.getProperty("java.ext.dirs").split(";")).
+                forEach(System.out::println);
+
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<AppClassCloader>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Stream.of(System.getProperty("java.class.path").split(";")).
+                forEach(System.out::println);
+    }
+
+    @Test
+    public void test2 () {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+    }
+}
