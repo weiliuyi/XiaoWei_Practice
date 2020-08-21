0c1f3395b5c1e4f1c7f4138f24ca31c6a7a213d6
weiliuyi <weiliuyi@58.com>
2020-08-21 10:37:17 +0800

    插入排查

--- src/main/java/example/algorithm/interview/order/InsertOrder.java
+++ src/main/java/example/algorithm/interview/order/InsertOrder.java
@@ -1,5 +1,7 @@
 package example.algorithm.interview.order;
 
+import java.util.Arrays;
+
 /**
  * @description: 插入排序
  * @author: weiliuyi
@@ -8,15 +10,25 @@ package example.algorithm.interview.order;
 public class InsertOrder {
 
     public static void main(String[] args) {
-
+        int[] array = {5, 3, 1, 9, 7, 6, 4, 2, 0};
+        insertOrder(array);
+        System.out.printf(Arrays.toString(array));
     }
 
-    private static void insertOrder (int[] array)  {
-
+    private static void insertOrder(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > value) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = value;
+        }
     }
 
     /**
-     *  交换数组角标的两个值
+     * 交换数组角标的两个值
      */
     private static void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
---
+++ src/main/java/example/base/juc/MapTest.java
@@ -0,0 +1,18 @@
+package example.base.juc;
+
+import java.util.HashMap;
+
+/**
+ * @description:
+ * @author: weiliuyi
+ * @create: 2020--20 19:27
+ **/
+public class MapTest {
+    public static void main(String[] args) {
+        HashMap mapTest = new HashMap();
+        mapTest.put("wly","wly_test");
+
+    }
+}
+
+
