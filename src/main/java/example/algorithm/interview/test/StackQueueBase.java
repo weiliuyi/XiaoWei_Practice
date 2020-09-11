5011e0a75a22f786fc1d3cecd1ed262ed6f020de
weiliuyi <weiliuyi@58.com>
2020-09-11 16:01:22 +0800

    滑动窗口

---
+++ src/main/java/example/algorithm/interview/test/StackQueueBase.java
@@ -0,0 +1,34 @@
+package example.algorithm.interview.test;
+
+/**
+ * @description: 使用数组和链表实现栈和队列
+ * @author: weiliuyi
+ * @create: 2020--11 11:31
+ *
+ **/
+public class StackQueueBase {
+
+}
+
+
+/**
+ * 双向链表的节点
+ *
+ */
+class LinkNode {
+    private int num;
+    LinkNode last;
+    LinkNode next;
+    public LinkNode (int num) {
+        this.num = num;
+    }
+}
+
+class MyLinkQueue {
+    private LinkNode head;
+    private LinkNode tail;
+
+
+
+
+}
