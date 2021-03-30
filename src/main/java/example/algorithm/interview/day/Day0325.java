89abfaa88a58cf75d05a6493798c0ab1ce13578d
weiliuyi <weiliuyi@58.com>
2021-03-30 19:12:59 +0800

    1.单调栈

--- src/main/java/example/algorithm/interview/day/Day0325.java
+++ src/main/java/example/algorithm/interview/day/Day0325.java
@@ -1,20 +1,23 @@
 package example.algorithm.interview.day;
 
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 /**
  * 单调栈相关的算法
- *
+ * <p>
  * 此题的暴力求解比较简单--忽略
  */
 public class Day0325 {
 
     @Test
-    public void test () {
+    public void test() {
         int[] result = {2, 1, 2, 4, 3};
         System.out.println("beforeGreaterEle = " + Arrays.toString(beforeGreaterEle(result)));
         System.out.println("nextGreaterEle = " + Arrays.toString(nextGreaterEle(result)));
@@ -23,15 +26,22 @@ public class Day0325 {
 
 
     @Test
-    public void testTemperatures () {
+    public void testTemperatures() {
         //比如说给你输入T = [73,74,75,71,69,76]，你返回[1,1,3,2,1,0]。
-        int[] temp = new int[] {73,74,75,71,69,76};
+        int[] temp = new int[]{73, 74, 75, 71, 69, 76};
         System.out.println("dailyTemperatures = " + Arrays.toString(dailyTemperatures(temp)));
+    }
+
+    @Test
+    public void testNextGreaterCirculation () {
+        int[] temp = new int[]{2,1,2,4,3};
+        System.out.println("testNextGreaterCirculation = " + Arrays.toString(nextGreaterEleCirculation(temp)));
     }
 
 
     /**
      * 前一个比它大的元素
+     *
      * @param arr 数组
      * @return
      */
@@ -51,6 +61,14 @@ public class Day0325 {
                 stack.push(arr[i]);
                 result[i] = -1;
             }
+            /*
+             * 逻辑优化
+             * while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+             *     stack.pop();
+             * }
+             * result = stack.isEmpty() ? -1 : stack.peek();
+             * stack.push(arr[i]);
+             */
         }
         return result;
     }
@@ -58,11 +76,11 @@ public class Day0325 {
     /**
      * 下一个比它大的元素数组 从前向后进行遍历
      */
-    private int[] nextGreaterEle (int[] arr) {
+    private int[] nextGreaterEle(int[] arr) {
         int[] result = new int[arr.length];
 
         Stack<Integer> indexStack = new Stack<>();//存放数据的下标，角标对数的数组从大到小的顺序排列
-        for (int i = 0;i < arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             while (!indexStack.isEmpty()) {//如果栈不为null
                 if (arr[indexStack.peek()] >= arr[i]) { //栈顶元素大于此arr[i]，押入栈并且跳出循环
                     indexStack.push(i);
@@ -76,7 +94,7 @@ public class Day0325 {
                 indexStack.push(i);
             }
         }
-        while (!indexStack.isEmpty()){
+        while (!indexStack.isEmpty()) {
             result[indexStack.peek()] = -1;
             indexStack.pop();
         }
@@ -91,7 +109,7 @@ public class Day0325 {
         int[] result = new int[arr.length];
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = arr.length-1;i >= 0;i--) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             while (!stack.isEmpty()) {
                 if (stack.peek() > arr[i]) {
                     result[i] = stack.peek();
@@ -104,38 +122,76 @@ public class Day0325 {
                 stack.push(arr[i]);
                 result[i] = -1;
             }
+            /*
+             * 对上述逻辑进行优化
+             * while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+             *     stack.pop();
+             * }
+             * result[i] = stack.isEmpty() ? -1 : result.peek;
+             * stack.push(arr[i]);
+             */
         }
         return result;
     }
 
-/**
- * 给你一个数组T，这个数组存放的是近几天的天气气温，你返回一个等长的数组
- * ，计算：对于每一天，你还要至少等多少天才能等到一个更暖和的气温；如果等不到那一天，填 0
- */
+    /**
+     * 给你一个数组T，这个数组存放的是近几天的天气气温，你返回一个等长的数组
+     * ，计算：对于每一天，你还要至少等多少天才能等到一个更暖和的气温；如果等不到那一天，填 0
+     */
 
-private int[] dailyTemperatures (int[] temperatures) {
-    int[] result = new int[temperatures.length];
-    Stack<Integer> indexStack = new Stack<>();
+    private int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> indexStack = new Stack<>();
 
-    for (int i = temperatures.length-1; i >=0; i--) {
+        for (int i = temperatures.length - 1; i >= 0; i--) {
 
-        while (!indexStack.isEmpty()) {
-            if (temperatures[indexStack.peek()] > temperatures[i]) {
-                result[i] = indexStack.peek() - i;
-                indexStack.push(i);
-                break;
+            while (!indexStack.isEmpty()) {
+                if (temperatures[indexStack.peek()] > temperatures[i]) {
+                    result[i] = indexStack.peek() - i;
+                    indexStack.push(i);
+                    break;
+                }
+                indexStack.pop();
             }
-            indexStack.pop();
+            if (indexStack.isEmpty()) {
+                indexStack.push(i);
+            }
+            /*
+             * 这一步逻辑进行优化
+             * while (!indexStack.isEmpty() && temperatures[indexStack.peek()] <= temperatures[i]) {
+             *     indexStack.pop();
+             * }
+             * result[i] = indexStack.isEmpty() ? 0 ? indexStack.peek()-i;
+             * indexStack.push(i);
+             */
         }
-
-        if (indexStack.isEmpty()) {
-            indexStack.push(i);
-        }
+        return result;
     }
-    return result;
-}
 
+    /**
+     * 样是 Next Greater Number，现在假设给你的数组是个环形的，如何处理？
+     */
+    public int[] nextGreaterEleCirculation(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[arr.length];
+        for (int i = arr.length * 2 - 1; i >= 0; i--) {
+            int temp = arr[i % arr.length];
+            while (!stack.isEmpty() && stack.peek() <= temp)  stack.pop();
+            result[i % arr.length] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(temp);
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        String id = "3272015757443495305";
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            list.add(id);
+        }
+        String content = JSON.toJSONString(list);
+        System.out.printf("result = " + content.length());
+    }
 
 
 }
