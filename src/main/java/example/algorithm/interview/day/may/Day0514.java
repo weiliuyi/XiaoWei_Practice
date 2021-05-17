d2dbc99dfa7031c95603d50a287c6e6a4193d4a5
weiliuyi <weiliuyi@58.com>
2021-05-17 20:27:54 +0800

    1.打印二叉树的边界

--- src/main/java/example/algorithm/interview/day/may/Day0514.java
+++ src/main/java/example/algorithm/interview/day/may/Day0514.java
@@ -4,7 +4,9 @@ import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 /**
  * @ClassName Day0514 打印二叉树的边界
@@ -44,7 +46,7 @@ public class Day0514 {
         root.left = l2;
         root.right = r2;
 
-        TNode  lr3= new TNode(4);
+        TNode lr3 = new TNode(4);
         l2.right = lr3;
 
         TNode rl3 = new TNode(5);
@@ -79,20 +81,191 @@ public class Day0514 {
         rlll5.right = rlllr6;
 
 
+        AbstractEdgeTree abTreeV1 = new PrintEdgeTreeV1();
+        List<Integer> res = abTreeV1.getTreeEdgeRes(root);
+        System.out.println(JSON.toJSONString(res));
 
-        List<Entry> entryList = getLevelLREntry(root);
-        for (Entry entry: entryList) {
-            System.out.print(" "+ entry.l.data);
-            System.out.print(" " + entry.r.data);
-            System.out.println(" ");
+        System.out.println("---------------------");
+
+        AbstractEdgeTree abTreeV2 = new PrintEdgeTreeV12();
+        List<Integer> res2 = abTreeV2.getTreeEdgeRes(root);
+        System.out.println(JSON.toJSONString(res2));
+        //System.out.println(getHeight(root, 0));
+        //System.out.println(getHeightV2(root));
+
+        System.out.println("---------------------------");
+
+        printEdgeTreeV2 abTree = new printEdgeTreeV2();
+        System.out.println(JSON.toJSONString(abTree.getTreeEdgeRes(root)));
+
+    }
+
+
+    /**
+     * 按照标准二的方式进行打印的树的边界；
+     */
+    private class printEdgeTreeV2  {
+
+
+        private List<Integer> getTreeEdgeRes(TNode root) {
+            List<Integer> res = new ArrayList<>();
+            List<Integer> left = getLeftNode(root);
+            List<Integer> right = getRightBoundNodes(root);
+            res.addAll(left);
+            res.addAll(getBottomNodes(root,left,right));
+            res.addAll(right);
+            return res;
         }
 
-        List<Integer> result = printEdgeV1(root);
-        System.out.println(JSON.toJSON(result));
+        private List<Integer> getBottomNodes (TNode root,List<Integer> left,List<Integer> right) {
+            List<Integer> res = new ArrayList<>();
+            fillBottomBoundNode2Res(root,left,right,res);
+            return res;
+        }
+
+        private void fillBottomBoundNode2Res (TNode root,List<Integer> left,List<Integer> right,List<Integer> res) {
+            if (root == null) return ;
+
+            if (root.left == null && root.right == null && !left.contains(root.data) && !right.contains(root.data)) {
+                res.add(root.data);
+            }
+            fillBottomBoundNode2Res(root.left,left,right,res);
+            fillBottomBoundNode2Res(root.right,left,right,res);
+        }
+
+        private List<Integer> getLeftNode (TNode root) {
+            List<Integer> res = new ArrayList<>();
+            fillLeftBoundNodes2Res(root,res);
+            return res;
+        }
+
+        private void fillLeftBoundNodes2Res (TNode root,List<Integer> res) {
+            if (root == null) return;
+
+            res.add(root.data);
+            if (root.left != null) {
+                fillLeftBoundNodes2Res(root.left,res);
+            } else {
+                fillLeftBoundNodes2Res(root.right,res);
+            }
+        }
+
+        private List<Integer> getRightBoundNodes (TNode root) {
+            List<Integer> res= new ArrayList<>();
+            fillRightBoundNode2Res(root,res);
+            return res;
+        }
+
+        private void fillRightBoundNode2Res (TNode root,List<Integer> res) {
+            if (root == null) return;
+
+            if (root.right != null) {
+                fillRightBoundNode2Res(root.right,res);
+            } else {
+                fillLeftBoundNodes2Res(root.left,res);
+            }
+            res.add(root.data);
+        }
+
 
 
     }
 
+
+    private class Entry {
+        TNode l, r;
+
+        public Entry(TNode l, TNode r) {
+            this.l = l;
+            this.r = r;
+        }
+    }
+
+    /**
+     * 使用数组的方式
+     */
+    private class PrintEdgeTreeV12 extends AbstractEdgeTree<TNode[][]> {
+
+        @Override
+        List<Integer> getLeftBoundNodes(TNode[][] level) {
+            List<Integer> result = new ArrayList<>();
+            for (TNode[] tNodes : level) {
+                result.add(tNodes[0].data);
+            }
+            return result;
+        }
+
+        @Override
+        List<Integer> getLeafBoundNodes(TNode root, TNode[][] levelR) {
+            List<Integer> res = new ArrayList<>();
+            Set<Integer>  lrNodeSet = Arrays.stream(levelR).flatMap(Arrays::stream).map(en -> en.data).collect(toSet());
+            fillBottom2Res(root,res,lrNodeSet);
+            return res;
+        }
+
+        private void fillBottom2Res (TNode root,List<Integer> res,Set<Integer> lrNodeSet) {
+            if (root == null) return;
+
+            if (root.left == null && root.right == null && !lrNodeSet.contains(root.data)) {
+                res.add(root.data);
+            }
+            fillBottom2Res(root.left,res,lrNodeSet);
+            fillBottom2Res(root.right,res,lrNodeSet);
+        }
+
+        @Override
+        List<Integer> getRightBoundNodes(TNode[][] level) {
+            List<Integer> res = new ArrayList<>();
+            for (int i = level.length-1; i >= 1 ; i--) {
+                res.add(level[i][1].data);
+            }
+            return res;
+        }
+
+        @Override
+        TNode[][] getLevelLR(TNode root) {
+            int height = getHeight(root,0);
+            TNode[][] res = new TNode[height][2];
+            fillLevelLr2Res(root,res,0);
+            return res;
+        }
+
+        private void fillLevelLr2Res (TNode root,TNode[][] res,int level) {
+            if (root == null) return;
+
+            if (res[level][0] == null ) {
+                res[level][0] = root;
+            }
+            res[level][1] = root;
+
+            fillLevelLr2Res(root.left,res,level + 1);
+            fillLevelLr2Res(root.right,res,level + 1);
+        }
+
+
+
+        /**
+         * 获得二叉树层数
+         * TODO 注意这个参数返回
+         */
+        private int getHeight(TNode root, int l) {
+            if (root == null) return l;
+
+            return Math.max(getHeight(root.left, l + 1), getHeight(root.right, l + 1));
+        }
+
+
+        /**
+         * 这个逻辑很好理解：
+         * 左右子树的中最高层数加一
+         */
+        private int getHeightV2(TNode root) {
+            if (root == null) return 0;
+
+            return Math.max(getHeightV2(root.left), getHeightV2(root.right)) + 1;
+        }
+
+    }
 
     /**
      * 问题分析：
@@ -119,100 +292,135 @@ public class Day0514 {
      * 完成逆时针的打印；
      */
 
-
-    private List<Integer> printEdgeV1(TNode root) {
-
-        //打印左侧结点
-        List<Entry> entryList = getLevelLREntry(root);
-
-        // 所有层的最左和最右集合；
-        List<Integer> bounds = getTreeBound(entryList);
-        List<Integer> result = new ArrayList<>(getLeftBound(entryList));
-        fillResultLeafNode(bounds,root,result);
-        result.addAll(getRightBound(entryList));
-        return result;
-    }
+    private class PrintEdgeTreeV1 extends AbstractEdgeTree<List<Entry>> {
 
 
-    /**
-     * 叶子结点不包括最左和最右结点
-     */
-    private void fillResultLeafNode(List<Integer> col, TNode root, List<Integer> res) {
-
-        if (root == null) return;
-
-        if (root.left == null && root.right == null && !col.contains(root.data)) {
-            res.add(root.data);
+        /**
+         * 左边界结点
+         */
+        @Override
+        List<Integer> getLeftBoundNodes(List<Entry> level) {
+            return level.stream().map(entry -> entry.l.data).collect(toList());
         }
-        fillResultLeafNode(col,root.left,res);
-        fillResultLeafNode(col,root.right,res);
-    }
 
-
-    private List<Integer> getLeftBound(List<Entry> entries) {
-        List<Integer> result = entries.stream().map(entry -> entry.l.data).collect(Collectors.toList());
-        return result;
-    }
-
-    private List<Integer> getRightBound(List<Entry> entries) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = entries.size()-1; i >= 1 ; i--) {
-            result.add(entries.get(i).r.data);
+        /**
+         * 叶子结点不包括最左和最右结点
+         */
+        @Override
+        List<Integer> getLeafBoundNodes(TNode root, List<Entry> levelR) {
+            List<Integer> res = new ArrayList<>();
+            Set<Integer> lRNodeList = levelR.stream().map(en -> new Integer[]{en.l.data, en.r.data}).flatMap(Arrays::stream).collect(toSet());
+            fillResBottomNode(root, res, lRNodeList);
+            return res;
         }
-        return result;
-    }
 
 
-    /**
-     * 获得所有所有的边界结点
-     */
-    private List<Integer> getTreeBound (List<Entry> entries) {
-        return entries.stream().
-                map(entry -> new Integer[]{entry.l.data, entry.r.data}).
-                flatMap(Arrays::stream).collect(Collectors.toList());
-    }
+        /**
+         * 叶子结点不包括最左和最右结点
+         */
+        private void fillResBottomNode(TNode root, List<Integer> res, Set<Integer> lRNodeSet) {
+            if (root == null) return;
 
-    /**
-     * 如何解决最左侧的元素和最右侧元素？？？
-     * 最右侧元素：
-     * 上层元素遍历到最后一个时，可以确定下层最右侧元素
-     * 最左侧元素：
-     * 也就是：上层元素结束之后遍历到的第一个元素；
-     */
-    private List<Entry> getLevelLREntry(TNode root) {
-        List<Entry> result = new ArrayList<>();
-        Deque<TNode> queue = new LinkedList<>();
-        queue.push(root);
-        TNode flag = root;
-        result.add(new Entry(root,root));
-        while (!queue.isEmpty()) {
-            TNode temp = queue.poll();
-            TNode right = temp.right;
-            TNode left = temp.left;
-            if (left != null) {
-                queue.add(left);
-            }
-            if (right != null) {
-                queue.add(right);
+            if (root.left == null && root.right == null && !lRNodeSet.contains(root.data)) {
+                res.add(root.data);
             }
 
-            if (flag == temp) {
-                if (queue.isEmpty()) break;
-                flag = queue.peekLast();//最右侧的元素解决了
-                result.add(new Entry(queue.peekFirst(),flag));
-            }
+            fillResBottomNode(root.left, res, lRNodeSet);
+            fillResBottomNode(root.right, res, lRNodeSet);
         }
-        return result;
+
+
+        /**
+         * 右边界结点
+         */
+        @Override
+        List<Integer> getRightBoundNodes(List<Entry> level) {
+            List<Integer> result = new ArrayList<>();
+            for (int i = level.size() - 1; i >= 1; i--) {
+                result.add(level.get(i).r.data);
+            }
+            return result;
+        }
+
+
+        /**
+         * 如何解决最左侧的元素和最右侧元素？？？
+         * 最右侧元素：
+         * 上层元素遍历到最后一个时，可以确定下层最右侧元素
+         * 最左侧元素：
+         * 也就是：上层元素结束之后遍历到的第一个元素；
+         */
+        @Override
+        List<Entry> getLevelLR(TNode root) {
+            List<Entry> result = new ArrayList<>();
+            Deque<TNode> deque = new LinkedList<>();
+            deque.add(root);
+            result.add(new Entry(root,root));
+            TNode flag = root;
+            while (!deque.isEmpty()) {
+                TNode node = deque.poll();
+                TNode left = node.left;
+                TNode right = node.right;
+                if (left != null) deque.add(left);
+
+                if (right != null) deque.add(right);
+
+                if (flag == node) {
+                    if (deque.isEmpty()) break;
+                    flag = deque.peekLast();
+                    result.add(new Entry(deque.peekFirst(), flag));
+                }
+            }
+            return result;
+        }
+
+
+    }
+
+    private abstract class AbstractEdgeTree<T> {
+
+        /**
+         * 左边界
+         */
+        abstract List<Integer> getLeftBoundNodes(T level);
+
+        /**
+         * 底部边界
+         */
+        abstract List<Integer> getLeafBoundNodes(TNode root, T levelR);
+
+        /**
+         * 右部边界
+         */
+        abstract List<Integer> getRightBoundNodes(T level);
+
+        abstract T getLevelLR(TNode root);
+
+
+        /**
+         * 打拼树的边界结点的骨架
+         */
+        public List<Integer> getTreeEdgeRes(TNode root) {
+            ArrayList<Integer> result = new ArrayList<>();
+            T levelR = getLevelLR(root);
+            result.addAll(getLeftBoundNodes(levelR));
+            result.addAll(getLeafBoundNodes(root, levelR));
+            result.addAll(getRightBoundNodes(levelR));
+            return result;
+        }
+
     }
 
 
-    private class Entry {
-        TNode l, r;
-
-        public Entry(TNode l, TNode r) {
-            this.l = l;
-            this.r = r;
-        }
+    @Test
+    public void testTemp() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.stream().forEach(System.out::println);
     }
 
     private class TNode {
